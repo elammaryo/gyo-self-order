223fb64a9848ae5c52f10901727543419cb6cd66
elammaryo <122314668+elammaryo@users.noreply.github.com>
2024-12-29 06:27:19 -0500

    Create side_bar.dart

---
+++ lib/components/side_bar.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:gyo/api/backend_api.dart';
+import 'package:gyo/models/MenuCategory.dart';
+import 'package:gyo/pages/menu_items_page.dart';
+import 'package:gyo/shared/styles.dart';
+
+class SideBar extends StatelessWidget {
+  const SideBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.2,
+      height: MediaQuery.of(context).size.height,
+      color: Colors.orange[100],
+      child: FutureBuilder(
+        future: BackendAPI.getMenuCategories(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: const CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            List<MenuCategory> categories = snapshot.data ?? [];
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: ListView.builder(
+                itemCount: categories.length,
+                itemBuilder: (context, index) => Column(
+                  children: [
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.orange[200],
+                        fixedSize: Size(
+                            MediaQuery.of(context).size.width * 0.2 - 40, 90),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        padding: const EdgeInsets.all(15),
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                        Navigator.pop(context);
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MenuItemsPage(
+                                    itemList: categories[index].items ?? [])));
+                      },
+                      child: Column(
+                        spacing: 5,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            height: 40,
+                            width: 40,
+                            child: Image.asset(categories[index].image ?? ''),
+                          ),
+                          Text(categories[index].name ?? 'Category',
+                              style: poppinsFont16ptBlack),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                  ],
+                ),
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
